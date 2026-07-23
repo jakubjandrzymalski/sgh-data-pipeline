1496af89f639c658c6cfc01a846d408f343745f3
Jakub Drzymalski <jakubjandrzymalski@gmail.com>
2026-07-23 18:39:04 +0200

    feat: make transform.py fully dynamic and fault-tolerant

--- transform.py
+++ transform.py
@@ -1,50 +1,71 @@
 import os 
 import json
+import glob 
 import pandas as pd
 
-# open the file for reading with right encoding
-with open('data/raw/AAPL_daily.json', 'r', encoding='utf-8') as f:
-    raw_data = json.load(f)
-
-# skip the metadata and extract only the stock market data
-time_series = raw_data.get("Time Series (Daily)", {})
+# search for files that match the pattern
+input_files = glob.glob("data/raw/*_daily.json")
 
 
-# load data into pandas + transposition
-df = pd.DataFrame(time_series).T
+for file_path in input_files:
 
-# reset index dispite date
-df =df.reset_index()
+    # open the file for reading with right encoding
+    with open(file_path, 'r', encoding='utf-8') as f:
+        raw_data = json.load(f)
 
-# columns name cleaning 
-df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
+    # skip the metadata and extract only the stock market data
+    time_series_key = None
 
-# data test
-# print(df.head())
+    for key in raw_data.keys():
+        if "Time Series" in key:
+            time_series_key = key
+            break
+    
+    time_series = raw_data.get(time_series_key, {})
 
-
-
-
-# converting data from a string to a number
-df["open"] = df["open"].astype(float)
-df["high"] = df["high"].astype(float)
-df["low"] = df["low"].astype(float)
-df["close"] = df["close"].astype(float)
-
-df["volume"] = df["volume"].astype(int)
-
-# data type test
-# print(df.dtypes)
+    # if no data inside
+    if not time_series:
+        print(f"Skipping the file {file_path}: no valid time series data!!!")
+        continue
 
 
-# adding a column to identify the company
-df["symbol"] = "AAPL"
+    # load data into pandas + transposition
+    df = pd.DataFrame(time_series).T
 
-# rearranging the order of the columns
-df = df[["date", "symbol", "open", "high", "low", "close", "volume"]]
+    # reset index dispite date
+    df =df.reset_index()
 
-print(df.head())
+    # columns name cleaning 
+    df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
 
-# check if destination folder exists. If it not, we create it
-os.makedirs('data/processed/AAPL_daily.csv',  exist_ok=False)
+    # data test
+    # print(df.head())
+
+    # converting data from a string to a number
+    df["open"] = df["open"].astype(float)
+    df["high"] = df["high"].astype(float)
+    df["low"] = df["low"].astype(float)
+    df["close"] = df["close"].astype(float)
+    df["volume"] = df["volume"].astype(float)
+
+    # data type test
+    # print(df.dtypes)
+
+
+    # adding a column to identify the company
+    base_name = os.path.basename(file_path)
+    symbol = base_name.split("_")[0]
+    df["symbol"] = symbol
+
+    # rearranging the order of the columns
+    df = df[["date", "symbol", "open", "high", "low", "close", "volume"]]
+
+    # print(df.head())
+
+    # check if destination folder exists. If it not, we create it
+    os.makedirs('data/processed', exist_ok=True)
+    output_path = f"data/processed/{symbol}_daily.csv"
+    df.to_csv(output_path, index=False)
+
+    print(f"Successfully processed data: {symbol} -> {output_path}")
 
